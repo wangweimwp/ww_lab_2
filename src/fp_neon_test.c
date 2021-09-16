f146ede5a8874c3955b19a204c832725f7fef193
hikvision pc <929244872@qq.com>
2021-09-16 19:15:26 +0800

    ld2 ld3 实现memcpy

--- src/fp_neon_test.c
+++ src/fp_neon_test.c
@@ -3,6 +3,8 @@
 
 static unsigned char a[MEMCPY_SIZE] = {0};
 static unsigned char b[MEMCPY_SIZE] = {0};
+static unsigned char c[MEMCPY_SIZE] = {0};
+static unsigned char d[MEMCPY_SIZE] = {0};
 
 
 void my_fp_neon_test(void)
@@ -16,8 +18,24 @@ void my_fp_neon_test(void)
 
 	for(i = 0; i < MEMCPY_SIZE; i ++){
 		if(a[i] != b[i]){
-			printk("memcpy err\n");
+			printk("ld1 memcpy err\n");
 			while(1);
+		}
+	}
+
+	neon_test_ld2(a, c, MEMCPY_SIZE);
+	for(i = 0; i < MEMCPY_SIZE; i++){
+		if(a[i] != c[i]){
+			printk("ld2 memcpy err\n");
+			while(1);
+		}
+	}
+
+	neon_test_ld3(a, d, MEMCPY_SIZE);
+	for(i = 0; i < MEMCPY_SIZE; i ++){
+		if(a[i] != d[i]){
+		printk("ld2 memcpy err\n");
+		while(1);
 		}
 	}
 }
